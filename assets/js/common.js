2b1109b3c5d5d1b7f8e6ca690f7e070592373c8c
kyoku <favorita.sanada@gmail.com>
2021-10-23 22:58:10 +0900

    リンク、イベントAPI

--- assets/js/common.js
+++ assets/js/common.js
@@ -29,4 +29,33 @@ $(function () {
   $(".l-spmenu_inner").on("click", function (e) {
     e.stopPropagation();
   });
+
+  $.ajax("https://connpass.com/api/v1/event/?series_id=6256&order=2&count=1", {
+    type: "get",
+    dataType: "jsonp",
+  }).done(function (data) {
+    const eventData = data.events[0];
+    const weeks = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const fulldate = new Date(eventData.started_at);
+    const fulldateEnd = new Date(eventData.ended_at);
+    const year = fulldate.getFullYear();
+    const month = fulldate.getMonth() + 1;
+    const date = fulldate.getDate();
+    const day = weeks[fulldate.getDay()];
+    const startHour = String(fulldate.getHours()).padStart(2, "0");
+    const startMinute = String(fulldate.getMinutes()).padStart(2, "0");
+    const endHour = String(fulldateEnd.getHours()).padStart(2, "0");
+    const endMinute = String(fulldateEnd.getMinutes()).padStart(2, "0");
+
+    const eventDate = `${year}.${month}.${date}(${day}) ${startHour}:${startMinute}〜${endHour}:${endMinute}`;
+
+    const eventTitle = eventData.title;
+    const eventLink = eventData.event_url;
+
+    $(".js-eventDate").text(eventDate);
+    $(".js-eventTitle").text(eventTitle);
+    $(".js-eventLink").attr("href", eventLink);
+
+    FONTPLUS.reload();
+  });
 });
